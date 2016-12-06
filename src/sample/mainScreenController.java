f510b46e879788c989d881f9addd41c7624df828
FloBWer <bueschersflorian@gmail.com>
2016-12-06 16:06:40 +0100

    Alle Tags übernommen

--- src/sample/mainScreenController.java
+++ src/sample/mainScreenController.java
@@ -1,4 +1,71 @@
 package sample;
 
-public class Controller {
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ListView;
+import javafx.scene.control.SplitMenuButton;
+import javafx.scene.control.TextField;
+
+public class mainScreenController {
+
+
+    @FXML
+    Button gamesSpielen;
+    @FXML
+    Button gamesZurPlaylistHinzufügen;
+    @FXML
+    SplitMenuButton gamesSpielerEins;
+    @FXML
+    SplitMenuButton gamesSpielerZwei;
+    @FXML
+    Button gamesPlaylistStarten;
+    @FXML
+    ListView gamesAktuellePlaylist;
+    @FXML
+    ListView gamesVerfuegbareSpiele;
+    @FXML
+    TextField benutzerTextBoxAnlegen;
+    @FXML
+    Button benutzerErstellen;
+    @FXML
+    SplitMenuButton benutzerAuswahlZuAendern;
+    @FXML
+    Button benutzerLoeschen;
+    @FXML
+    TextField benutzerTextBoxAendern;
+    @FXML
+    Button benutzerAendernSpeichern;
+
+
+
+    @FXML
+    public void clickBenutzerErstellen(ActionEvent event){
+
+    }
+
+    @FXML
+    public void clickBenutzerLoeschen(ActionEvent event){
+
+    }
+    @FXML
+    public void clickBenutzerAendernSpeichern(ActionEvent event){
+
+    }
+
+    @FXML
+    public void clickZurPlaylistHinzufuegen(ActionEvent event){
+
+    }
+
+    @FXML
+    public void clickGamesSpielen(ActionEvent event){
+
+    }
+
+    @FXML
+    public void clickPlaylistStarten(ActionEvent event){
+
+    }
 }
+
