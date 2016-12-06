f012cb661461a556d8f4e0ac34695a948083128e
FloBWer <bueschersflorian@gmail.com>
2016-12-06 16:56:57 +0100

    Controller für normale Revancheabfrage

---
+++ src/windows/revancheController.java
@@ -0,0 +1,27 @@
+package windows;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
+/**
+ * Created by Florian on 06.12.2016.
+ */
+public class revancheController {
+    @FXML
+    Button revancheJa;
+    @FXML
+    Button revancheNein;
+
+
+    @FXML
+    public void clickRevancheJa(ActionEvent event){
+
+    }
+
+    @FXML
+    public void clickRevancheNein(ActionEvent event){
+
+    }
+
+}
