19b8ab15e3fe1ef4d588ef480a4c69bd0c8010b4
FloBWer <bueschersflorian@gmail.com>
2016-12-06 17:09:46 +0100

    Controller für Playlist Ergebnisanzeige mit Revancheabfrage

---
+++ src/windows/playlistErgebnisRevancheController.java
@@ -0,0 +1,29 @@
+package windows;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TableView;
+
+/**
+ * Created by Florian on 06.12.2016.
+ */
+public class playlistErgebnisRevancheController {
+    @FXML
+    Button playlistRevancheJa;
+    @FXML
+    Button playlistRevancheNein;
+    @FXML
+    TableView tabelleErgebnis;
+
+    @FXML
+    public void clickPlaylistRevancheJa(ActionEvent event){
+
+    }
+    @FXML
+    public void clickPlaylistRevancheNein(ActionEvent event){
+
+    }
+
+}
+
