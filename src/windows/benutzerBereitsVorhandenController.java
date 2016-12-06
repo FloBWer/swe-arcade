e03f5e71c090f42560e83e299a83f9ffbc6dd9b4
FloBWer <bueschersflorian@gmail.com>
2016-12-06 19:41:44 +0100

    Controller für die Warnmeldung, falls ein Benutzer bereits existiert

---
+++ src/windows/benutzerBereitsVorhandenController.java
@@ -0,0 +1,21 @@
+package windows;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
+
+/**
+ * Created by Florian on 06.12.2016.
+ */
+public class benutzerBereitsVorhandenController {
+
+
+    @FXML
+    Button bereitsVorhandenOK;
+
+    public void clickBereitsVorhandenOK(ActionEvent event){
+        Stage stage = (Stage) bereitsVorhandenOK.getScene().getWindow();
+        stage.close();
+    }
+}
