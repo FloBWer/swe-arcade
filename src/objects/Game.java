6a3ad6b2dca895eb2de2bd828187cc5519bf5fe1
FloBWer <bueschersflorian@gmail.com>
2016-12-08 09:35:41 +0100

    Objekte

---
+++ src/objects/Game.java
@@ -0,0 +1,35 @@
+package objects;
+
+import java.io.File;
+import java.util.*;
+
+/**
+ * Created by Florian on 08.12.2016.
+ */
+public class Game {
+    //Attrbute
+    private String name;
+    private String pfad;
+
+    //Methoden
+    public Game (String name, String pfad){
+        this.name=name;
+        this.pfad=pfad;
+    }
+
+    public static List<Game> readGamesFolder(){
+        List<Game> gamesInFolder=new ArrayList<>();
+        File f=new File("C:\\Users\\Florian\\Desktop\\Games"); //nur als Test, muss noch angepasst werden
+        File[] fileArray = f.listFiles();
+        for(File gelesen : fileArray){
+            if(gelesen.getName().endsWith(".jar")){
+                String nameAktuell=gelesen.getName().substring(0, (gelesen.getName().length()-4));
+                String pfadAktuell=gelesen.getAbsolutePath();
+                Game aktuellesSpiel=new Game(nameAktuell,pfadAktuell);
+                gamesInFolder.add(aktuellesSpiel);
+            }
+        }
+        return gamesInFolder;
+    }
+
+}
