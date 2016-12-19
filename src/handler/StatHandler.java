6ec382356e3374c1c5175a6193d102b4d423ee46
Shakreo09 <acid1990@gmail.com>
2016-12-19 19:09:26 +0100

    added some methods for models

--- src/handler/StatHandler.java
+++ src/handler/StatHandler.java
@@ -1,8 +1,11 @@
 package handler;
 
+import com.google.gson.Gson;
 import objects.StatRow;
+import objects.User;
 
 import java.util.HashMap;
+import java.util.List;
 
 /**
  * Created by Shakreo on 18.12.2016.
@@ -10,7 +13,24 @@ import java.util.HashMap;
 public class StatHandler {
   private HashMap stats;
 
-  public StatHandler(String gameList) {
+  public StatHandler() {
     stats = new HashMap<String, StatRow>();
+  }
+
+  public void addGame(String game, String statString) {
+    Gson g = new Gson();
+    StatRow row = g.fromJson(statString, StatRow.class);
+    stats.put(game, row);
+  }
+
+  public void newGame(String game, List<User> users) {
+    stats.put(game, new StatRow(users));
+  }
+
+  public boolean containsGame(String game) {
+    return stats.containsKey(game);
+  }
+
+  public void updateStats() {
   }
 }
--- src/objects/StatEntry.java
+++ src/objects/StatEntry.java
@@ -7,4 +7,10 @@ public class StatEntry {
   private String user;
   private int wins;
   private int looses;
+
+  public StatEntry(String user, int wins, int looses) {
+    this.user = user;
+    this.wins = wins;
+    this.looses = looses;
+  }
 }
